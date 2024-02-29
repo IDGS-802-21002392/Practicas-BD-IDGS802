03e99f61fda376ceb5e18731900b3361dad9fd1e
IDGS-802-21002392 <dianaHdez0119@gmail.com>
2024-02-29 12:11:41 -0600

    first commit

--- forms.py
+++ forms.py
@@ -16,9 +16,10 @@ class UserForm3 (Form):
     id = IntegerField('id', [validators.number_range(min=1, max=20, message='valor no valido')])
     nombre = StringField("nombre", [
         validators.DataRequired(message='campo es requerido'), 
-        validators.length(min=4, max=10, message='ingrese un nombre valido')
+        validators.Length(min=4, max=10, message='ingrese un nombre valido')
     ])
     email = EmailField('correo', [validators.Email(message='ingrese un correo valido')])
     apaterno = StringField('apaterno')
-    amaterno = StringField('apaterno')
-    materias = StringField('apaterno')
+    amaterno = StringField('amaterno')
+    materias = StringField('materias')
+
--- main.py
+++ main.py
@@ -14,34 +14,37 @@ csrf= CSRFProtect()
 
 @app.route("/index", methods=["GET", "POST"])
 def index():
-    alum_form = forms.UserForm2(request.form)
-    mae_form = forms.UserForm3(request.form)
-
-    if request.method == 'POST':
-        if alum_form.validate():  # Asumiendo que quieres validar y agregar solo si el formulario de alumno es válido
-            alum = Alumnos(nombre=alum_form.nombre.data, apaterno=alum_form.apaterno.data,
-                           email=alum_form.email.data)
-            db.session.add(alum)  # Agrega el objeto alumno a la sesión de la base de datos
-            
-        if mae_form.validate():  # Similarmente, valida y agrega el maestro solo si su formulario es válido
-            mae = Maestros(nombre=mae_form.nombre.data, apaterno=mae_form.apaterno.data,
-                           amaterno=mae_form.amaterno.data, email=mae_form.email.data, materias=mae_form.materias.data)
-            db.session.add(mae)  # Agrega el objeto maestro a la sesión de la base de datos
-
-        db.session.commit()  # Commit una vez después de agregar todos los objetos necesarios
-
-    # Corregir la llamada a render_template para usar nombres de argumento únicos
-    return render_template("index.html", form =alum_form, form1=mae_form)
-
+    alum_form=forms.UserForm2(request.form)
+    if request.method=='POST' and alum_form.validate():
+        alum=Alumnos(nombre=alum_form.nombre.data, apaterno = alum_form.apaterno.data,
+                 email = alum_form.email.data)
+        #insert into alumnos values()
+        db.session.add(alum)
+        db.session.commit()
+    return render_template("index.html", form=alum_form)
 
 @app.route("/ABC_Completo", methods=["GET", "POST"])
 def ABCCompleto():
     alum_form = forms.UserForm2(request.form)
     alumno = Alumnos.query.all()
-    mae_form = forms.UserForm3(request.form)
+
+    return render_template("ABC_Completo.html", Alumnos=alumno)
+
+@app.route("/index1", methods=["GET", "POST"])
+def maestro():
+    mae_form=forms.UserForm3(request.form)
+    if request.method=='POST' and mae_form.validate():
+        mae=Maestros(nombre=mae_form.nombre.data, apaterno = mae_form.apaterno.data, amaterno=mae_form.amaterno.data,
+                 email = mae_form.email.data, materias = mae_form.materias.data)
+        db.session.add(mae)
+        db.session.commit()
+    return render_template("index1.html", form=mae_form)
+
+@app.route("/ABC_Completo1", methods=["GET", "POST"])
+def ABCCompleto1():
     maestro = Maestros.query.all()
 
-    return render_template("ABC_Completo.html", alumno=alumno, maestro = maestro)
+    return render_template("ABC_Completo1.html", Maestros=maestro)
 
 @app.route("/alumnos", methods=["GET", "POST"])
 def index1():
